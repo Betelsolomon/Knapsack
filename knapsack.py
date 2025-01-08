2715a3fa0736c180fa8354221e5e9fb077299a23
beth <bebrave1214@gmail.com>
2025-01-08 09:28:02 +0300

    Backtrack

--- knapsack.py
+++ knapsack.py
@@ -1,10 +1,21 @@
 def knapsack(values, weights, W):
     n = len(values)
     dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
-      for i in range(1, n + 1):
+    for i in range(1, n + 1):
         for w in range(W + 1):
             if weights[i - 1] <= w:
                 dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
             else:
                 dp[i][w] = dp[i - 1][w]
+    max_value = dp[n][W]
+    selected_items = []
+    w = W
+
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            selected_items.append(i - 1)  
+            w -= weights[i - 1]
+
+    selected_items.reverse()
+    return max_value, selected_items
   
